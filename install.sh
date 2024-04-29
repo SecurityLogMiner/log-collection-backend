e565c14dc60ccda0e8714c663303645431035a8d
Joe Murche <73696931+murchej@users.noreply.github.com>
2024-04-29 15:00:32 -0700

    terraform template, needs editing

--- install.sh
+++ install.sh
@@ -14,3 +14,23 @@ else
     echo "Log directory already exists at: $LOG_DIR"
 fi
 
+# Set the desired Terraform version
+TERRAFORM_VERSION="1.2.9"
+
+# Download Terraform. Adjust the version number as necessary.
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# Unzip the downloaded file
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# Move the executable to a directory included in the system's PATH
+sudo mv terraform /usr/local/bin/
+
+# Remove the downloaded ZIP file
+rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# Check the installation
+terraform --version
+
+# Print success message
+echo "Terraform installed successfully."
